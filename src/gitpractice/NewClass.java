e0dc45977bbff94631a45a0158477d52e141d820
giddalurvenkateswarlu <161282913+giddalurvenkateswarlu@users.noreply.github.com>
2024-03-15 11:43:11 +0530

    Update NewClass.java

--- src/gitpractice/NewClass.java
+++ src/gitpractice/NewClass.java
@@ -5,6 +5,9 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello       RBG33'");
+		System.out.println("Hello       RBG33'");
+		System.out.println("Hello       RBG33'");
+		System.out.println("Hello       RBG44'");
 
 	}
 	
